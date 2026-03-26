fc9d9928635dce2b6a187fd7cf20c5f13b8219e8
wladchk <wladchk@mail.com>
2026-03-26 19:14:42 -0400

    second done

--- app/main.py
+++ app/main.py
@@ -6,19 +6,13 @@ def format_linter_error(error: dict) -> dict:
         "name": error["code"],
         "source": "flake8"
     }
-error = {
-    "code": "E501",
-    "filename": "./source_code_2.py",
-    "line_number": 18,
-    "column_number": 80,
-    "text": "line too long (99 > 79 characters)",
-    "physical_line": '    return f"I like to filter, rounding, doubling, store and decorate numbers: {\', \'.join(items)}!"',
-}
-print(format_linter_error(error))
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path, "status": "failed" if errors else "passed"
+    }
+
 
 
 def format_linter_report(linter_report: dict) -> list:
