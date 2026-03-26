bb25522c8265256b03c8798d43e6bbda82a2c131
wladchk <wladchk@mail.com>
2026-03-26 19:17:28 -0400

    third done

--- app/main.py
+++ app/main.py
@@ -16,5 +16,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
+    ]
